d48a421c10e142678fb6abe79303f0a3c296f63f
Vaibhav Sompura <vsompura3@gmail.com>
2023-04-16 21:54:54 +0530

    experimenting with the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Home } from './pages/Home'
-import NotFound from './pages/NotFound'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/layout/Header";
+import { Home } from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,36 +1,35 @@
-const navItems = [
-  { idx: 0, linkTitle: 'Login', path: '' },
-  { idx: 1, linkTitle: 'Register', path: '' },
-]
-
-const Link = ({ children, href }) => {
-  return (
-    <a
-      href={href}
-      className="rounded px-4 py-2 text-lg focus-within:bg-slate-700 focus-within:text-white focus-within:outline-offset-2 hover:bg-slate-700 hover:text-white"
-    >
-      {children}
-    </a>
-  )
-}
-
 const Header = () => {
   return (
-    <header className="p-8 text-slate-800">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between gap-4">
-        <a href="/" className="text-3xl font-black text-teal-800">
-          Commentate
-        </a>
-        <ul className="flex items-center gap-4">
-          {navItems.map(({ idx, linkTitle, path }) => (
-            <li key={idx}>
-              <Link href={path}>{linkTitle}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  )
-}
+    <>
+      <div className="border-b border-gray-300 bg-gray-100">
+        <div className="container mx-auto px-5">
+          <p className="py-2 text-center text-sm">
+            The source code for this blog is{" "}
+            <a
+              href="https://github.com/vsompura3/commentate-blog-application"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-success underline transition-colors duration-200"
+            >
+              {" "}
+              available on GitHub.
+            </a>{" "}
+            PRs are welcome.
+          </p>
+        </div>
+      </div>
+      <header className="container mx-auto px-5">
+        <div className="mb-16 mt-16 flex flex-col items-center md:mb-12 md:justify-between lg:flex-row">
+          <h1 className="text-5xl font-bold leading-tight tracking-tighter sm:text-6xl md:text-8xl lg:pr-8">
+            Commentate.
+          </h1>
+          <p className="mt-5 text-center text-xl md:text-left lg:pl-8">
+            Discover stories, thinking, and expertise from writers on any topic.
+          </p>
+        </div>
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
